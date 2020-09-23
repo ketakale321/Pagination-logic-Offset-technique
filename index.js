98e7b24d39f622517a3df291e578794d5e5e5f6a
Ketan Takale <takaleketan321@gmail.com>
2020-09-23 16:04:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// --------------PAGINATION PRACTISE-----------------
+
+let data = require('./data');
+let data_length = data.length;
+
+function paginate(page, limit) {
+  // if requested page number is greater than what we have then always load the last page
+  let total_pages = Math.round(data_length / limit);
+  if (page > total_pages) {
+    console.log('Data for requested page is not available. Loading data for last page...')
+    page = total_pages;
+  }
+
+  let pageNumber = page <= 0 ? 1 : page;
+  let itemsPerPage = limit <=0 ? 1: limit;
+  let itemsAfter = (pageNumber - 1) * itemsPerPage;
+
+  // restrict the user to view data till the last available index
+  let endOn = (itemsAfter + itemsPerPage) < data_length ? (itemsAfter + itemsPerPage) : data_length;
+
+  console.log("itemsAfter", pageNumber, total_pages, itemsAfter, itemsAfter + itemsPerPage, endOn);
+
+  return data.slice(itemsAfter, endOn)
+}
+
+// console.log(JSON.stringify(paginate(1,10)));
+
+// console.log(JSON.stringify(paginate(0,0)));
+
+// console.log(JSON.stringify(paginate(201,10)));
+
+// Please put custom validations on negative values of page number and limit
+// console.log(JSON.stringify(paginate(-1, -1)));
+
